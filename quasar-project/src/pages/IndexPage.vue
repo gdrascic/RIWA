02da42177e37f0d5cadfb05e9f62ef471d52a1d2
gdrascic <gabriel.drascic@gmail.com>
2025-10-20 11:49:53 +0200

    Kreirane nove stranice

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -1,8 +1,8 @@
 <template>
   <q-page class="flex flex-center">
     <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
+      alt="Knjižnica"
+      src="~assets/knjiznica.jpg"
       style="width: 200px; height: 200px"
     />
   </q-page>
